632b80a2efed38da1cd954562dfcb4ca7b73a607
Damien GABRIELLE <damien.gabrielle@epsi.fr>
2014-04-30 17:49:20 +0200

    Update dim.h
    Modification des imports

--- cuda_blur/cuda_blur/dim.h
+++ cuda_blur/cuda_blur/dim.h
@@ -1,22 +1,14 @@
 #ifndef _DIM_H_
 
 #include <iostream>
-#include <stdio.h>
-#include <cuda.h>
-#include <device_functions.h>
-
-#pragma once
-#ifdef __INTELLISENSE__
-void __syncthreads();
-#endif
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "device_functions.h"
 #include "common\cpu_bitmap.h"
 #include "common\book.h"
 
-#define DIM 400
-#define DIM_2 4	
+#define DIM 512
+#define DIM_2 2
 #define DEBUG false
 
 #endif
