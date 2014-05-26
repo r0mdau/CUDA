6ae829b28ae7f56d8b6f884f739bd19e157c3d8b
Romain Dauby <romain.dauby@gmail.com>
2014-05-26 12:33:35 +0200

    Debut pour la rotation de l'image

--- CUDA/sphere.cu
+++ CUDA/sphere.cu
@@ -10,7 +10,8 @@
 #include "sphere.h"
 
 #define DIM 512
-#define NUM_SPHERES 300
+#define NUM_SPHERES 10
+texture<unsigned char, 2> texConstSrc;
 
 struct Sphere{
 	int r, g ,b;
@@ -53,6 +54,22 @@ __global__ void kernel (Sphere * tab, unsigned char *ptr) {
 		}
 	}
 }
+__global__ void kernelRotate (unsigned char *ptr, float deg) {
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int offset = x + y * blockDim.x * gridDim.x;
+	tex2D(texConstSrc, x, y);
+}
+
+__device__ float readPixVal( unsigned char* ImgSrc,int ImgWidth,int x,int y)
+{
+    return (float)ImgSrc[y*ImgWidth+x];
+}
+
+__device__ void putPixVal( unsigned char* ImgSrc,int ImgWidth,int x,int y, float floatVal)
+{
+    ImgSrc[y*ImgWidth+x] = floatVal;
+}
 
 void mainSphere (void) {
 	srand(time(NULL));
@@ -62,7 +79,7 @@ void mainSphere (void) {
 
 	Sphere tab[NUM_SPHERES];
 	for(int i = 0; i < NUM_SPHERES; i++){
-		tab[i].init(rand() % 256, rand() % 256, rand() % 256, rand() % DIM, rand() % DIM, rand() % DIM, rand () % 50);
+		tab[i].init(rand() % 256, rand() % 256, rand() % 256, DIM/2, rand() % DIM, rand() % DIM, 30);
 	}
 
 	cudaMalloc( (void**)&scene, sizeof(Sphere) * NUM_SPHERES);
@@ -73,22 +90,13 @@ void mainSphere (void) {
 	dim3 grid(DIM/16, DIM/16);
 	dim3 threads(16, 16);
 	
-	// DEBUT calcul du temps d'exécution du floutage
-	cudaEvent_t start,stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord(start, 0);
-
 	kernel<<<grid, threads>>>(scene, dev_bitmap);
 
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop);
-	float elapsedTime;
-	cudaEventElapsedTime(&elapsedTime, start, stop);
-	printf(" Temps de generation %3.1f ms\n", elapsedTime);
-	cudaEventDestroy(start);
-	cudaEventDestroy(start);
-	// FIN calcul du temps d'exécution du floutage
+	cudaMalloc((void**)&texConstSrc,  DIM * DIM * 4);
+	cudaChannelFormatDesc desc = cudaCreateChannelDesc<unsigned char>();
+	cudaBindTexture2D( NULL, texConstSrc, dev_bitmap, desc, DIM, DIM, DIM * DIM * 4 );
+
+	kernelRotate<<<grid, threads>>>(dev_bitmap, 45);
 
 	cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost);	
 
